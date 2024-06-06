5e7b8d509b44e5c3013cec92a2afa00b8ac5ec3c
Anastasia Dyvnich <anastasiaduvnich@gmail.com>
2024-06-06 18:08:31 +0300

    Add matrix (buster)

--- spaceship/routers/api.py
+++ spaceship/routers/api.py
@@ -1,8 +1,20 @@
 from fastapi import APIRouter
+import numpy as np
 
 router = APIRouter()
-
 
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get('/matrix')
+def generate_and_multiply_matrices() -> dict:
+    matrix_a = np.random.randint(0, 100, size=(10, 10)).tolist()
+    matrix_b = np.random.randint(0, 100, size=(10, 10)).tolist()
+    product = np.dot(matrix_a, matrix_b).tolist()
+
+    return {
+        "matrix_a": matrix_a,
+        "matrix_b": matrix_b,
+        "product": product
+    }
